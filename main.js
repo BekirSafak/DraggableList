1d971caf9599a5b11ec357dc9a40b82270646ee2
Bekir ŞAFAK <bekir_safak_38@hotmail.com>
2022-12-29 22:34:50 +0300

    Sortable için JS kullanımı yazıldı. CSS'de iyileştirme yapıldı.

---
+++ main.js
@@ -0,0 +1,8 @@
+// ? Sortable özelliğinin kullanımı.
+let list = document.querySelector('.container');
+// * Sıralamak istediğimiz öğelerin kapsayıcısı tanımlanmalı.
+
+new Sortable(list, {
+    animation: 500
+})
+// ? Sortable özelliğinin kullanımı.
